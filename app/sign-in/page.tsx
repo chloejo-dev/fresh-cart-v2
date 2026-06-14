6410aaf3ff8e9cdee94aa86db0e0e33a0ac3c2ac
Chloe Jo <hyeyoonjo321@gmail.com>
2026-06-14 16:45:05 -0300

    feat(sign in): add sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,19 +1,37 @@
 "use client";
-import styles from "./page.module.css";
 import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <main>
-      <div className={styles.inputContainer}>
-        <label>Username:</label>
-        <input name='username'></input>
-        <label>Password:</label>
-        <input name='password'></input>
+      <div className={styles.signInContainer}>
+        <div className={styles.inputContainer}>
+          <div className={styles.formGroup}>
+            <label>Username:</label>
+            <input
+              type='text'
+              name='username'
+              required
+              autoComplete='username'
+            ></input>
+            <label>Password:</label>
+            <input
+              type='password'
+              name='password'
+              required
+              autoComplete='current-password'
+            ></input>
+          </div>
+        </div>
+        <button type='submit' className={styles.signInBtn}>
+          Sign In
+        </button>
+        <div className={styles.signUpContainer}>
+          <span>Don&apos;t have an account?</span>
+          <Link href='/sign-up'>Sign Up</Link>
+        </div>
       </div>
-      <button type='submit'>Sign In</button>
-      <span>Don&apos;t have account?</span>
-      <Link href='/sign-up'>Sign Up</Link>
     </main>
   );
 }
