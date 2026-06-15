0f21cd405989e43d555c6b39babdd71b374f530d
Chloe Jo <hyeyoonjo321@gmail.com>
2026-06-15 17:10:58 -0300

    feat(sign in): implement sign in API

--- app/api/sign-in/route.ts
+++ app/api/sign-in/route.ts
@@ -1,1 +1,98 @@
-export function GET() {}
+import { NextResponse } from "next/server"; // Similar to res.json in Express
+import db from "@/lib/db";
+import type { RowDataPacket } from "mysql2";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  try {
+    // Get user info from frontend
+    const { username, password } = await request.json();
+
+    // Validate type before normalizing the info
+    if (typeof username !== "string" || typeof password !== "string") {
+      return NextResponse.json(
+        { message: "Invalid username or password" },
+        { status: 400 },
+      );
+    }
+
+    // Normalize the info
+    const normalizedUsername = username.trim().toLowerCase();
+
+    // Validate the info
+    if (
+      normalizedUsername.includes(" ") ||
+      normalizedUsername.length < 5 ||
+      password === ""
+    ) {
+      return NextResponse.json(
+        { message: "Invalid username or password" },
+        { status: 400 },
+      );
+    }
+
+    // Check if username exists in DB
+    const [rows] = await db.query<RowDataPacket[]>(
+      "SELECT user_id, hashed_password FROM users WHERE username = ?",
+      [normalizedUsername],
+    );
+
+    // No user found with the username
+    if (rows.length === 0) {
+      return NextResponse.json(
+        { message: "Invalid username or password" },
+        { status: 401 },
+      );
+    }
+
+    // Validate if password  === hashed_password
+    const hashedPassword = rows[0].hashed_password;
+
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+
+    // No, inform frontend
+    if (!isMatch) {
+      return NextResponse.json(
+        { message: "Invalid username or password" },
+        { status: 401 },
+      );
+    }
+
+    // Yes, create a sign-in session
+    const userId = rows[0].user_id;
+
+    // JWT
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    // Store JWT in cookies
+    const cookieStore = await cookies();
+
+    cookieStore.set("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24,
+    });
+
+    return NextResponse.json({ message: "Sign in success" }, { status: 200 });
+  } catch (err: unknown) {
+    console.error(err);
+    return NextResponse.json(
+      {
+        message: "User sign in failed",
+      },
+      { status: 500 },
+    );
+  }
+}
