65911f5c319e931ddd85a5fe184719c35a12d607
Chloe Jo <hyeyoonjo321@gmail.com>
2026-06-16 19:24:14 -0300

    feat(account): add protected account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+type User = {
+  username: string;
+  email: string;
+  joined_date: string;
+};
+
 export default function Page() {
-  return <div>Account Page</div>;
+  const [currentUser, setCurrentUser] = useState<User>();
+  const [signInErr, setSignInErr] = useState<string>("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Make a GET request to fetch current user info
+    const fetchUser = async () => {
+      // User sign in?
+      const res = await fetch("/api/me");
+      // N:
+      if (!res.ok) {
+        console.log("Please sign in");
+        router.push("/sign-in");
+        return;
+      }
+      // Y:
+      const data = await res.json();
+      setCurrentUser(data);
+      console.log(data);
+    };
+
+    fetchUser();
+  }, []);
+
+  if (!currentUser) {
+    return <p>Please sign in...</p>;
+  }
+
+  return (
+    <div>
+      <p>{currentUser.username}</p>
+      <p>{currentUser.email}</p>
+      <p>{currentUser.joined_date}</p>
+    </div>
+  );
 }
