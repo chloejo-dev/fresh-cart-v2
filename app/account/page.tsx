bcedeb5cecd2bb938047ae1c9c5d06d81a3b0aa2
Chloe Jo <hyeyoonjo321@gmail.com>
2026-06-18 10:40:53 -0300

    feat(account): add api/me API

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 type User = {
   username: string;
@@ -11,7 +12,7 @@ type User = {
 
 export default function Page() {
   const [currentUser, setCurrentUser] = useState<User>();
-  const [signInErr, setSignInErr] = useState<string>("");
+  // const [signInErr, setSignInErr] = useState<string>("");
 
   const router = useRouter();
 
@@ -33,17 +34,42 @@ export default function Page() {
     };
 
     fetchUser();
-  }, []);
+  }, [router]);
 
   if (!currentUser) {
     return <p>Please sign in...</p>;
   }
 
   return (
-    <div>
-      <p>{currentUser.username}</p>
-      <p>{currentUser.email}</p>
-      <p>{currentUser.joined_date}</p>
-    </div>
+    <main className={styles.main}>
+      <h1>Your Account</h1>
+      <div className={styles.userInfoContainer}>
+        <Image
+          src='/images/profile-picture.png'
+          alt='default user profile picture'
+          width={200}
+          height={200}
+        />
+        <div className={styles.group}>
+          <p>Username: {currentUser.username}</p>
+          <p>Email: {currentUser.email}</p>
+          <p>Member Since: {currentUser.joined_date}</p>
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button
+          type='submit'
+          className={`${styles.button} ${styles.primaryButton}`}
+        >
+          Logout
+        </button>
+        <button
+          type='submit'
+          className={`${styles.button} ${styles.secondaryButton}`}
+        >
+          Change Password
+        </button>
+      </div>
+    </main>
   );
 }
