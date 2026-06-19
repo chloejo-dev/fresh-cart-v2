30f3b48ed4e5a6360461440da50a15a3d5b7e73b
Chloe Jo <hyeyoonjo321@gmail.com>
2026-06-19 09:41:58 -0300

    feat(auth): add sign out API

---
+++ app/api/sign-out/route.ts
@@ -0,0 +1,12 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  // Delete cookie
+  const cookieStore = await cookies();
+  cookieStore.delete("auth_token");
+  return NextResponse.json(
+    { message: "Signed out successfully" },
+    { status: 200 },
+  );
+}
