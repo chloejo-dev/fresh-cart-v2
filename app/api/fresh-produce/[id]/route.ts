1d90ad38b721b6857e720a6e23775454946137e6
Chloe Jo <hyeyoonjo321@gmail.com>
2026-06-20 10:46:56 -0300

    refactor(auth): replace hard-coded user with JWT authentication

--- app/api/fresh-produce/[id]/route.ts
+++ app/api/fresh-produce/[id]/route.ts
@@ -1,15 +1,31 @@
 import { NextResponse } from "next/server"; // Similar to res.json in Express
 import db from "@/lib/db";
 import type { RowDataPacket } from "mysql2";
+import getUserIdFromToken from "@/lib/auth";
 
 export async function GET(
-  request: Request,
+  _request: Request,
   { params }: { params: Promise<{ id: string }> },
 ) {
-  const { id } = await params;
-  const userName = "testUser";
-  console.log("ID: ", id);
   try {
+    // Get product id
+    const { id } = await params;
+    const productId = Number(id);
+
+    if (!Number.isInteger(productId) || productId <= 0) {
+      return NextResponse.json(
+        { message: "Invalid product id" },
+        { status: 400 },
+      );
+    }
+
+    // Check if user has signed in. If yes, show product quantity stored in user's cart
+    const userId = await getUserIdFromToken();
+
+    // N:
+    if (userId === null) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
     // Check if product in user cart
     const [rows] = await db.query<RowDataPacket[]>(
       `SELECT
@@ -22,9 +38,9 @@ export async function GET(
         FROM products
         LEFT JOIN cart
         ON products.product_id = cart.product_id
-        AND cart.username=?
-        WHERE products.product_id=?`,
-      [userName, id],
+        AND cart.user_id= ?
+        WHERE products.product_id= ?`,
+      [userId, productId],
     );
 
     const product = rows[0];
@@ -40,13 +56,13 @@ export async function GET(
     return NextResponse.json(product);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      console.error(`GET /api/freshProduce/${id} failed`, err.message);
+      console.error(`GET /api/freshProduce/[id] failed`, err.message);
     } else {
-      console.error(`GET /api/freshProduce/${id} failed`, err);
+      console.error(`GET /api/freshProduce/[id] failed`, err);
     }
 
     return NextResponse.json(
-      { error: "Failed to fetch products" },
+      { message: "Failed to fetch products" },
       { status: 500 },
     );
   }
