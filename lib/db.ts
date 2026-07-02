7d19726a33968dabdbb3bffbe058e31c3d6ebd17
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-02 11:24:44 -0300

    fix(db): add configurable database port

--- lib/db.ts
+++ lib/db.ts
@@ -3,6 +3,7 @@ import mysql from "mysql2/promise";
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: Number(process.env.DB_PORT),
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
