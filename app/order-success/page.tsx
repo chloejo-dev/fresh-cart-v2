242a9f4c40afc8c51deac45488427d6e622c27c3
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-08 15:20:14 -0300

    style(order): polish order success page layout and styling

--- app/order-success/page.tsx
+++ app/order-success/page.tsx
@@ -3,8 +3,8 @@ import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <main className={styles.orderPage}>
-      <p>Order placed, thank you!</p>
+    <main className={styles.orderSuccessPage}>
+      <h1>Order placed, thank you!</h1>
       <p>Confirmation will be sent to your email.</p>
       <Link href='/'>Continue Shopping</Link>
     </main>
