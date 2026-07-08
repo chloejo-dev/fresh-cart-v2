665ede1d156d833a3eb798e64ab0ee0683a9ff5c
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-08 15:21:12 -0300

    feat(order): add view orders page

---
+++ app/orders/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Page() {
+  return (
+    <main className={styles.ordersPage}>
+      <h1>View Orders</h1>
+      <p>This feature is currently under development.</p>
+      <p>Please check back later.</p>
+    </main>
+  );
+}
