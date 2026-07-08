2727e71ecb07f1e68c2dfb0e6a46efc177a82b14
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-08 15:28:51 -0300

    feat(order): add reorder page

---
+++ app/reorder/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Page() {
+  return (
+    <main className={styles.reorderPage}>
+      <h1>Reorder</h1>
+      <p>This feature is currently under development.</p>
+      <p>Please check back later.</p>
+    </main>
+  );
+}
