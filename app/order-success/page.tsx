d9da1c92379fd02f51c8d8658ee9774fc62a528f
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-08 13:46:23 -0300

    feat(order): add order success page

--- app/order-success/page.tsx
+++ app/order-success/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <main>
+    <main className={styles.orderPage}>
       <p>Order placed, thank you!</p>
       <p>Confirmation will be sent to your email.</p>
       <Link href='/'>Continue Shopping</Link>
